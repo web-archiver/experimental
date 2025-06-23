26499f1c8c82cdcea9e4cd8337d4d9453a5ad678
Jose Lane <dariankline@outlook.com>
2025-06-24 07:50:07 +0800

    [core ] rs/codec/gcbor: add buf encoder

--- core/rs/src/codec/gcbor.rs
+++ core/rs/src/codec/gcbor.rs
@@ -8,11 +8,12 @@ like [crate::time::Timestamp] can be handled. For types in other crates that req
 manual encode and decode, define them here and add `#[doc(hidden)]` and
 reexport in that crate as a temporary measure.
  */
-use std::{borrow::Borrow, convert::Infallible};
+use std::{borrow::Borrow, convert::Infallible, fmt::Debug, marker::PhantomData, mem::transmute};
 
 #[doc(inline)]
 pub use self::internal::{cmp::GCborOrd, decoding::FromGCborSlice, encoding::ToGCbor};
 
+use ciborium_io::Write;
 /// Shorthand for deriving both [FromGCbor] and [ToGCbor]
 pub use webar_core_macros::GCborCodec;
 #[doc(inline)]
@@ -107,18 +108,19 @@ unsafe fn transmute_arr<const N: usize, S, D>(v: [S; N]) -> [D; N] {
 pub mod map;
 pub mod set;
 
-pub fn to_vec<T: ToGCbor + ?Sized>(value: &T) -> Vec<u8> {
-    struct VecWriter(Vec<u8>);
-    impl ciborium_io::Write for &mut VecWriter {
-        type Error = Infallible;
-        fn write_all(&mut self, data: &[u8]) -> Result<(), Self::Error> {
-            self.0.extend_from_slice(data);
-            Ok(())
-        }
-        fn flush(&mut self) -> Result<(), Self::Error> {
-            Ok(())
-        }
+struct VecWriter(Vec<u8>);
+impl ciborium_io::Write for &mut VecWriter {
+    type Error = Infallible;
+    fn write_all(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+        self.0.extend_from_slice(data);
+        Ok(())
     }
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+pub fn to_vec<T: ToGCbor + ?Sized>(value: &T) -> Vec<u8> {
     let mut writer = VecWriter(Vec::new());
     let mut encoder = ciborium_ll::Encoder::from(&mut writer);
     match value.encode(internal::encoding::Encoder(&mut encoder)) {
@@ -144,6 +146,75 @@ pub fn to_writer<T: ToGCbor + ?Sized>(
     value.encode(internal::encoding::Encoder(
         &mut ciborium_ll::Encoder::from(IoWriter(w)),
     ))
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SomeType();
+#[derive(Clone, PartialEq, Eq)]
+pub struct EncodedVal<T: ?Sized>(Vec<u8>, PhantomData<T>);
+impl<T: ?Sized + ToGCbor> EncodedVal<T> {
+    pub fn new(val: &T) -> EncodedVal<T> {
+        Self(to_vec(val), PhantomData)
+    }
+    pub fn untype(self) -> EncodedVal<SomeType> {
+        EncodedVal(self.0, PhantomData)
+    }
+}
+impl<T: ?Sized> ToGCbor for EncodedVal<T> {
+    fn encode<W: ciborium_io::Write>(
+        &self,
+        encoder: internal::encoding::Encoder<W>,
+    ) -> Result<(), internal::encoding::Error<W::Error>> {
+        encoder
+            .0
+            .write_all(&self.0)
+            .map_err(internal::encoding::Error)
+    }
+}
+
+pub struct ValueBuf(VecWriter);
+impl std::fmt::Debug for ValueBuf {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("ValueBuf")
+    }
+}
+#[derive(PartialEq, Eq)]
+pub struct ValueSlice<'a, T: ?Sized>(&'a [u8], PhantomData<T>);
+impl ValueBuf {
+    pub fn new() -> Self {
+        Self(VecWriter(Vec::new()))
+    }
+    pub fn encode<'a, T: ?Sized + ToGCbor>(&'a mut self, v: &T) -> ValueSlice<'a, T> {
+        self.0 .0.clear();
+        match v.encode(internal::encoding::Encoder(
+            &mut ciborium_ll::Encoder::from(&mut self.0),
+        )) {
+            Ok(()) => (),
+            Err(e) => match e.0 {},
+        };
+        ValueSlice(&self.0 .0, PhantomData)
+    }
+}
+impl<'a, T: ?Sized> ValueSlice<'a, T> {
+    pub fn untype(self) -> ValueSlice<'a, SomeType> {
+        ValueSlice(self.0, PhantomData)
+    }
+}
+impl<'a, T: ?Sized> AsRef<[u8]> for ValueSlice<'a, T> {
+    fn as_ref(&self) -> &[u8] {
+        self.0
+    }
+}
+impl<'a, T: ?Sized> ToGCbor for ValueSlice<'a, T> {
+    fn encode<W: Write>(
+        &self,
+        encoder: internal::encoding::Encoder<W>,
+    ) -> Result<(), internal::encoding::Error<W::Error>> {
+        encoder
+            .0
+            .write_all(self.0)
+            .map_err(internal::encoding::Error)
+    }
 }
 
 pub type DecodeSliceError = internal::decoding::Error;
