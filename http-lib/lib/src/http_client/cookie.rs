251cff29755f30c6d771e7dd67c3483671053355
Jose Lane <dariankline@outlook.com>
2026-07-08 09:17:49 +0800

    [http-lib] http-client: check public suffix for cookies

--- http-lib/lib/src/http_client/cookie.rs
+++ http-lib/lib/src/http_client/cookie.rs
@@ -1,14 +1,22 @@
 mod cookie_editor;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct CookieStore(pub(super) cookie_store::CookieStore);
 impl CookieStore {
     pub fn new() -> Self {
-        Self(cookie_store::CookieStore::new())
+        Self(cookie_store::CookieStore::new_with_public_suffix(Some(
+            publicsuffix::List::from_bytes(include_bytes!("./cookie/public_suffix_list.dat"))
+                .unwrap(),
+        )))
     }
     pub fn from_cookie_editor_json(json: &[u8]) -> anyhow::Result<Self> {
-        let mut store = cookie_store::CookieStore::new();
-        cookie_editor::import_json(&mut store, json)?;
-        Ok(Self(store))
+        let mut store = Self::new();
+        cookie_editor::import_json(&mut store.0, json)?;
+        Ok(store)
+    }
+}
+impl Default for CookieStore {
+    fn default() -> Self {
+        Self::new()
     }
 }
